1a6d7aebdc3b05dfa473863f43082684e3a57756
dantewawrykow-hash <dantewawrykow@gmail.com>
2025-11-05 20:17:41 -0500

    setting up repo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOp")
+public class Tele extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private BrainSTEMRobot robot;
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        robot = new BrainSTEMRobot(this.hardwareMap, this.telemetry, this);
+
+
+        waitForStart();
+
+
+        while (!opModeIsActive()) {
+
+            telemetry.addData("Opmode Status :", "Init");
+
+            telemetry.update();
+        }
+
+
+
+        while (opModeIsActive()) {
+            telemetry.update();
+
+
+            double y = -gamepad1.left_stick_y*0.4;
+            double x = gamepad1.left_stick_x*0.4;
+            double rx = gamepad1.right_stick_x*0.4;
+
+            robot.frontLeft.setPower(y + x + rx);
+            robot.backLeft.setPower(y - x + rx);
+            robot.frontRight.setPower(y - x - rx);
+            robot.backRight.setPower(y + x - rx);
+            telemetry.addData("frontLeft", robot.frontLeft.getPower());
+            telemetry.addData("frontRight", robot.frontRight.getPower());
+            telemetry.addData("backLeft", robot.backLeft.getPower());
+            telemetry.addData("backRight", robot.backRight.getPower());
+
+
+
+            telemetry.addData("y-axis :", y);
+            telemetry.addData("x-axis :", x);
+            telemetry.addData("turn :", rx);
+            //telemetry.update();
+
+
+        }
+    }
+}
