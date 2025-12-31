37cf233ecf0a491a6e7d5d1106a06bd662151540
Dhaniyasree <dhaniyasree2011@gmail.com>
2025-12-31 16:43:13 -0500

    updated auto actions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto_subsystems/AutoActions.java
@@ -0,0 +1,125 @@
+package org.firstinspires.ftc.teamcode.auto_subsystems;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+
+import org.firstinspires.ftc.teamcode.BrainSTEMAutoRobot;
+
+
+public class AutoActions {
+
+
+    private static BrainSTEMAutoRobot robot = null;
+    public static void setRobot(BrainSTEMAutoRobot robot) {
+        AutoActions.robot = robot;
+    }
+    public static Action setCollectorOn() {
+        return telemetryPacket -> {
+            robot.collector.collectorState = Collector.CollectorState.ON;
+            return false;
+        };
+    }
+
+    public static Action setCollectorOff() {
+        return telemetryPacket -> {
+            robot.collector.collectorState = Collector.CollectorState.OFF;
+            return false;
+        };
+    }
+
+    public static Action robotUpdate() {
+        return telemetryPacket -> {
+            robot.update();
+            return true;
+        };
+    }
+
+
+    public static Action shooterTurnOnFar() {
+        return telemetryPacket -> {
+            robot.shooter.shooterState = Shooter.ShooterState.SHOOT_FAR;
+            telemetryPacket.addLine("Shooter On");
+            return false;
+        };
+    }
+
+    public static Action shooterTurnOff() {
+        return telemetryPacket -> {
+            robot.shooter.shooterState = Shooter.ShooterState.OFF;
+            telemetryPacket.addLine("Shooter Off");
+            return false;
+        };
+    }
+//    public Action shooterMotorTwo(BrainSTEMAutoRobot robot) {
+//        return new Action() {
+//            @Override
+//            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+//                return false;
+//            }
+//        };
+//    }
+
+//    public Action shooterMotorOneOff(BrainSTEMAutoRobot robot) {
+//        return new Action() {
+//            @Override
+//            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+//                telemetryPacket.addLine("Shooter Off");
+//                return false;
+//
+//            }
+//        };
+//    }
+
+
+
+
+
+    public static Action moveSpindexer120() {
+        return moveSpindexer(Spindexer.normalRotateDeg);
+    }
+
+
+    public static Action moveSpindexer60() {
+        return moveSpindexer(Spindexer.shootRotateDeg);
+    }
+    private static Action moveSpindexer(double deg) {
+        return new Action() {
+            boolean first = true;
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                if (first) {
+                    robot.spindexer.rotateDegrees(deg);
+                    first = false;
+                }
+                telemetryPacket.addLine("indexer S3");
+                return !robot.spindexer.isStatic();
+            }
+        };
+    }
+
+    public static Action fingerServoU() {
+        return new SequentialAction (
+                telemetryPacket -> {
+                    robot.finger.fingerState = Finger.FingerState.UP;
+                    telemetryPacket.addLine("finger Up");
+                    return false;
+                },
+                new SleepAction(Finger.upTime)
+        );
+    }
+
+    public static Action fingerServoD() {
+        return new SequentialAction (
+                telemetryPacket -> {
+                    robot.finger.fingerState = Finger.FingerState.DOWN;
+                    telemetryPacket.addLine("finger Down");
+                    return false;
+                },
+                new SleepAction(Finger.downTime)
+        );
+    }
+}
